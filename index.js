bd53a48b45286a639df867eb7f7cded4d5630a89
unknown <pauleasal@alum.us.es>
2021-03-22 20:16:27 +0100

    Se ha añadido info/temperature-stats

--- index.js
+++ index.js
@@ -17,6 +17,64 @@ app.get('/cool', (request, response)=> {
     console.log("New request to /cool has arrived")
 })
 
+//Info temperature-stats: Paula
+app.get("/info/temperature-stats", (req, res) =>{
+    res.send(`<html>
+    <body>
+    <h2>Se trata de la temperatura media por pais</h2>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Country</th>
+      <th scope="col">Year</th>
+      <th scope="col">Temprature Min</th>
+      <th scope="col">Temperature Max</th>
+      <th scope="col">Temperature CO2</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">China</th>
+      <td>2010</td>
+      <td>37</td>
+      <td>46.6</td>
+      <td>40</td>
+    </tr>
+    <tr>
+    <th scope="row">Spain</th>
+    <td>2010</td>
+    <td>37</td>
+    <td>43</td>
+    <td>44</td>
+  </tr>
+  <tr>
+  <th scope="row">Canada</th>
+  <td>2010</td>
+  <td>37</td>
+  <td>44</td>
+  <td>46</td>
+    </tr>
+    <tr>
+    <th scope="row">Russia</th>
+    <td>2010</td>
+    <td>36</td>
+    <td>44</td>
+    <td>40</td>
+  </tr>
+  <tr>
+  <th scope="row">EEUU</th>
+  <td>2010</td>
+  <td>34</td>
+  <td>43</td>
+  <td>41</td>
+</tr>
+  </tbody>
+    </table>
+    </body>
+    </html>`)
+})
+
 app.listen(port, ()=>{
     console.log("Server ready listening on port "+port)
 })
