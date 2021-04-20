45a06790930c5927fd7e8213bc4d33751903b8b4
unknown <pauleasal@alum.us.es>
2021-04-20 17:12:41 +0200

    cambios aplicados

--- temperature_api/index.js
+++ temperature_api/index.js
@@ -87,10 +87,10 @@ module.exports.register = (app) => {
                 if (country == '' || typeof year == null || temperature_min == null || temperature_max == null || temperature_co2 == null) {
                     console.log("Invalid format of temperature.")
                     res.status(400).send("Invalid format of temperature.");
-                }else if (country == req.body.country || typeof year ==  req.body.year || temperature_min ==  req.body.temperature_min || temperature_max ==  req.body.temperature_max || temperature_co2 ==  req.body.temperature_co2){
+                 /*else if (country == req.body.country || typeof year ==  req.body.year || temperature_min ==  req.body.temperature_min || temperature_max ==  req.body.temperature_max || temperature_co2 ==  req.body.temperature_co2){
                         console.log("Conflict.")
                         res.sendStatus(409);
-                    
+                  */ 
                 } else {
                     new_temperature = {
                         country: country,
