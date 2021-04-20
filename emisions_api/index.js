1e32bae928e3a8be17eb445be349fd3000904a88
ALEJANDRO SOLIS MERCADO <alesolmer2@gmail.com>
2021-04-20 11:47:16 +0200

    Emisions Api modificado

--- emisions_api/index.js
+++ emisions_api/index.js
@@ -1,102 +1,174 @@
+var DataStore = require('nedb');
+var emisions_stats = new DataStore({ filename: 'emisions_api/emision.db', autoload: true });
 var BASE_API_PATH = "/api/v1";
 
+
 module.exports.register = (app) => {
-    var emisions_stats = [];
 
-app.get(BASE_API_PATH + "/emisions-stats", (req, res) => {
-    res.send(JSON.stringify(emisions_stats, null, 2));
-});
+    app.get(BASE_API_PATH + "/emisions-stats", (req, res) => {
+        dbquery = {}
+        if(req.query.country){
+            dbquery["country"] = req.query.country;
+        }
+        if(req.query.year){
+            dbquery["year"] = parseInt(req.query.year);
+        }
+        if(req.query.carb_diox_ppm){
+            dbquery["carb_diox_ppm"] = parseInt(req.query.carb_diox_ppm);
+        }
+        if(req.query.methane_ppb){
+            dbquery["methane_ppb"] = parseFloat(req.query.methane_ppb);
+        }
+        if(req.query.nitro_oxide_ppb){
+            dbquery["nitro_oxide_ppb"] = parseInt(req.query.nitro_oxide_ppb);
+        }
+        
+        const offset = parseInt(req.query.offset);
+        const limit = parseInt(req.query.limit);
+        delete offset;
+        delete limit;
 
-app.get(BASE_API_PATH + "/emisions-stats/loadInitialData", (req, res) => {
-    if (emisions_stats.length != 0) {
-        emisions_stats.splice(0, emisions_stats.length);
-    }
-    var emisionsIni = [{
-        "country": "spain",
-        "year": 2019,
-        "carb-diox-ppm": 377
-    },
-    {
-        "country": "japan",
-        "year": 2019,
-        "carb-diox-ppm": 379
-    }];
+        emisions_stats.find(dbquery).skip(offset).limit(limit).exec((err, docs) =>{
+            if(err){
+                console.log("[INFO] An unexcepted errer while loading database has ocurred.")
+                res.status(500).send("Error while loading the database")
+            }else{
+                if (docs.length == 0) {
+                    console.log("No hay datos");
+                    res.status(404).send("No existen datos");
+                } else {
+                    docs.forEach((doc) => {
+                        delete doc._id;
+                    })
+                    console.log("[INFO] Loading emisions")
+                    console.log(JSON.stringify(docs, null, 2))
+                    res.status(200).send(JSON.stringify(docs));
+                }
+            }
+        })
 
-    for (var emisions of emisionsIni){
-        emisions_stats.push(emisions);
-    }
-    res.status(201).send(JSON.stringify(emisions_stats, null, 2));
+        
 
-    console.log(`Nuevas emisiones creadas: <${JSON.stringify(emisionsIni, null, 2)}>`);
-    
-});
+    });
 
-app.post(BASE_API_PATH + "/emisions-stats", (req, res) => {
-    var newEmisions = req.body;
-    console.log(`Nuevas emisiones añadidas: <${JSON.stringify(newEmisions, null, 2)}>`);
-    emisions_stats.push(newEmisions);
-    res.sendStatus(201);
-});
-app.get(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
-    country = req.params.country
-    year = parseInt(req.params.year)
-    console.log("Buscando emisiones con año "+year+" y pais "+country);
-    for (var emisions of emisions_stats){
-		if (emisions.country == country && emisions.year == year){
-            console.log("Emisiones encontradas:\n"+JSON.stringify(emisions))
-			return res.status(200).json(emisions);
-		}
-	}
-    return res.sendStatus(404);
+    app.get(BASE_API_PATH + "/emisions-stats/loadInitialData", (req, res) => {
+        var emisionsIni = [
+            {
+                "country": "eeuu",
+                "year": 2019,
+                "carb_diox_ppm": 381.2,
+                "methane_ppb": 1782,
+                "nitro_oxide_ppb": 320.1
+            },
+            {
+                "country": "china",
+                "year": 2019,
+                "carb_diox_ppm": 389,
+                "methane_ppb": 1783,
+                "nitro_oxide_ppb": 318.6
+            },
+            {
+                "country": "spain",
+                "year": 2019,
+                "carb_diox_ppm": 377.1,
+                "methane_ppb": 1808,
+                "nitro_oxide_ppb": 323.2
+            }
+        ];
+        console.log('[INFO] Initial data loaded succesfully!');
+        emisions_stats.insert(emisionsIni)
+        res.status(201).send('Initial data loaded succesfully!');
+    });
 
-});
+    app.post(BASE_API_PATH + "/emisions-stats", (req, res) => {
+        country = req.body.country;
+        year = parseInt(req.body.year);
+        carb_diox_ppm = parseInt(req.body.carb_diox_ppm);
+        methane_ppb = parseFloat(req.body.methane_ppb);
+        nitro_oxide_ppb = parseInt(req.body.nitro_oxide_ppb);
+        emisions_stats.find({ $and: [{ "country": country, "year": year }] }, function (error, docs) {
+            if (docs.length > 0) {
+                console.log("[INFO] This emision already exists");
+                res.status(400).send("This emision already exists");
+            } else {
+                if (country == '' || year == 'null' || carb_diox_ppm == 'null' || methane_ppb == 'null' || nitro_oxide_ppb == 'null') {
+                    console.log("Invalid format of emision.")
+                    res.status(400).send("Invalid format of emision.");
+                } else {
+                    new_emision = {
+                        country: country,
+                        year: year,
+                        carb_diox_ppm: carb_diox_ppm,
+                        methane_ppb: methane_ppb,
+                        nitro_oxide_ppb: nitro_oxide_ppb
+                    }
+                    console.log('[INFO] New emision was added:\n' + JSON.stringify(new_emision));
+                    emisions_stats.insert(new_emision);
+                    res.status(201).send("New emision was added");
+                }
+            }
+        });
+    });
 
-app.delete(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
-    country = req.params.country
-    year = req.params.year
-    console.log("Eliminando emision con año "+year+" y pais "+country);
-    for (var i = 0; i < emisions_stats.length; i++) {
-		if (emisions_stats[i]["country"] == country && emisions_stats[i]["year"] == year) {
-            console.log("Recurso eliminado");
-			emisions_stats.splice(i, 1);
-			return res.status(200).send("Se ha eliminado el recurso");
-		}
-	}
+    app.get(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
+        country = req.params.country
+        year = parseInt(req.params.year)
+        console.log("[INFO] Searching for emision with year " + year + " and coutry name " + country)
+        emisions_stats.findOne({ $and: [{ country: country }, { year: year }] }, function (err, docs) {
+            res.sendStatus(200).send(docs);
+        });
+    });
 
-});
+    app.delete(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
+        country = req.params.country
+        year = parseInt(req.params.year)
+        emisions_stats.remove({ $and: [{ country: country }, { year: year }] }, function (err, docs) {
+            if (docs == 0) {
+                res.status(404).send("Emision not found")
+            } else {
+                res.status(200).send("Emision deleted.");
+            }
+        });
 
-app.put(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
-    country = req.params.country;
-    year = req.params.year;
-    console.log("Actualizando recurso con COUNTRY="+country+" y YEAR="+year);
-    data_updated = req.body;
-    if(emisions_stats.length == 0){
-        console.log("No hay datos");
-        return res.sendStatus(404);
-    } else {
-		for (var i = 0; i < emisions_stats.length; i++) {
-			if (emisions_stats[i].country == country && emisions_stats[i].year == year) {
-				emisions_stats[i] = data_updated;
-                console.log("Recurso actualizado");
-				return res.status(200).send("Elemento actualizado");
-			}
-		}
-    }
+    });
 
-});
-app.post(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
-    console.log("Acción no permitida");
-    res.sendStatus(405);
-});
+    app.put(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
+        countryQuery = req.params.country;
+        yearQuery = parseInt(req.params.year);
+        //Data updated
+        country = req.body.country;
+        year = parseInt(req.body.year);
+        carb_diox_ppm = parseInt(req.body.carb_diox_ppm);
+        methane_ppb = parseFloat(req.body.methane_ppb);
+        nitro_oxide_ppb = parseInt(req.body.nitro_oxide_ppb);
+        data_updated = {country, year, carb_diox_ppm, methane_ppb, nitro_oxide_ppb}
+        emisions_stats.update({ "country": countryQuery, "year": yearQuery }, { $set: data_updated }, (err, doc) => {
+            if (doc == 0) {
+                console.log("[INFO] Emision not found. Cannot be updated")
+                res.status(404).send("Emision not found.");
+            }
+            else {
+                console.log("[INFO] Emision updated")
+                res.status(200).send("Emision updated.");
+            }
+        });
 
-app.delete(BASE_API_PATH + "/emisions-stats", (req, res) => {
-    console.log("Datos borrados");
-    emisions_stats.splice(0, emisions_stats.length);
-    res.sendStatus(200);
-});
+    });
 
-app.put(BASE_API_PATH + "/emisions-stats", (req, res) => {
-    console.log("Acción no permitida");
-    res.sendStatus(405);
-});
+    app.post(BASE_API_PATH + "/emisions-stats/:country/:year", (req, res) => {
+        console.log("[INFO] Action not allowed");
+        res.sendStatus(405);
+    });
+
+    app.delete(BASE_API_PATH + "/emisions-stats", (req, res) => {
+        console.log("[INFO] All emisions were deleted");
+        emisions_stats.remove({}, { multi: true }, (error, docs) => {
+            res.status(200).send("All emisions were deleted");
+        });
+    });
+
+    app.put(BASE_API_PATH + "/emisions-stats", (req, res) => {
+        console.log("[INFO] Action not allowed");
+        res.sendStatus(405);
+    });
 }
