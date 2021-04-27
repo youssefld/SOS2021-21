6abd79b7039e96a4823533a3188c0c34fb558e5a
Youssef Lakoifat <youlakdar@alum.us.es>
2021-04-27 10:23:49 +0200

    Se ha añadido el enlace al front end de temperatura

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
 	import Router from 'svelte-spa-router';
     import FireAPI from './FireTable.svelte';
 	import Home from './Home.svelte';
-	import NotFound from './404.svelte'
+	import NotFound from './404.svelte';
+	import TemperatureAPI from './Temperature.svelte'
     const routes = {
 		"/":Home,
 		"/fire": FireAPI,
+		"/temperature": TemperatureAPI,
 		"*":NotFound
     };
 </script>
